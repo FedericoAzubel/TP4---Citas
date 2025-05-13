bf09e9560ee17cdcff5ac48c4cadabf79e7e4b91
FedericoAzubel <48677630@est.ort.edu.ar>
2025-05-13 08:05:02 -0300

    Added github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/TP4---Citas/',  // 👈 importante: usá el nombre exacto del repositorio
   plugins: [react()],
 })
